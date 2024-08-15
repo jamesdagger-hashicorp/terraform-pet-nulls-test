abcf1ea6c15cc65e76da8b84c2eeab3696739f9b
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2024-08-15 16:59:12 +0100

    sleep update

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "random_pet" "this" {
 }
 
 resource "time_sleep" "wait" {
-  create_duration = "1m"
+  create_duration = "30s"
 
   triggers = {
     # This will change whenever the pet changes, causing the sleep to occur
