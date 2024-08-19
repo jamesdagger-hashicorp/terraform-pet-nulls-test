410a4bb2bf2a090ebdf7638ccdb6b871d9dcaf71
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2024-08-19 14:44:19 +0100

    refactoring

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "name" {
   value = random_pet.this.id
 }
 
-output "ids" {
-  value = [for n in null_resource.this : n.id]
-}
+# output "ids" {
+#   value = [for n in null_resource.this : n.id]
+# }
